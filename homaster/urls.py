4ce9264829c1461b09f58442431ba219be1221e3
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-09-08 13:43:21 +0900

    ENGAWA新規作成時のバックエンド処理追加 GMをPlayerとしてDBに登録

--- homaster/urls.py
+++ homaster/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('engawa/<uuid>', views.EngawaView.as_view(), name='engawa'),
+    path('engawa/<uuid>/<p_code>', views.EngawaView.as_view(), name='engawa'),
 ]
--- homaster/views.py
+++ homaster/views.py
@@ -9,12 +9,19 @@ class IndexView(TemplateView):
 
     def post(self, request):
         data = request.POST
+
         # ENGAWAのUUID払い出し
         e_uuid = uuid.uuid4()
         # ENGAWAを作成
         Engawa.objects.create(uuid=e_uuid, scenario_name=data['scenario_name'])
+
+        # p_codeの払い出し
+        code = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+        # GMのPlayerレコードを登録
+        Player.objects.create(engawa=Engawa(uuid=e_uuid), p_code=code, gm_flag=True)
+
         # 管理画面にリダイレクト
-        return redirect(to=f'/engawa/{e_uuid}')
+        return redirect(to=f'/engawa/{e_uuid}/{code}')
 
 class EngawaView(ListView):
     template_name = 'homaster/engawa.html'
