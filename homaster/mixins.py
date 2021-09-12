dbaa49f2e93f99f9e16d1519ad8b40158bc96f08
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-09-13 08:05:32 +0900

    リダイレクトの修正

--- homaster/mixins.py
+++ homaster/mixins.py
@@ -5,5 +5,5 @@ class LoginRequiredCustomMixin(AccessMixin):
     """Verify that the current user is authenticated."""
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect(to='index')
+            return redirect(to='homaster:index')
         return super().dispatch(request, *args, **kwargs)
