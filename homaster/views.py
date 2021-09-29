ee89d4295398c14b7bcfe09041ed6c00df388264
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2021-09-29 16:10:58 +0900

    ハンドアウトが作成順に並ばないバグ対応

--- homaster/views.py
+++ homaster/views.py
@@ -198,7 +198,7 @@ class EngawaView(LoginRequiredCustomMixin, ListView):
 
     def get_queryset(self):
         engawa = self.request.user.engawa
-        return Handout.objects.filter(engawa=engawa).order_by('type')
+        return Handout.objects.filter(engawa=engawa).order_by('type', 'id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
