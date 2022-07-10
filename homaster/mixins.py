4139c4c862b0278d1ee38ec52bfd52265be205b2
Nagyon-Jessica <john.with.sloping.shoulders@gmail.com>
2022-07-10 08:16:59 +0000

    閲覧権限更新モーダルの集約&バリデーションの追加

--- homaster/mixins.py
+++ homaster/mixins.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import AccessMixin
 from django.shortcuts import redirect
 
+
 class LoginRequiredCustomMixin(AccessMixin):
     """Verify that the current user is authenticated."""
     def dispatch(self, request, *args, **kwargs):
